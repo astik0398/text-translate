2fa7f09f4bfe38435f0b3c209cd761d57a99851e
Kumar Astik <kumarastik0398@gmail.com>
2024-07-02 17:27:44 +0530

    text translation app speech to text feature implemented

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -2,6 +2,9 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "../styles/Translate.css";
 import volume from "../assets/volume.svg";
+import mic from "../assets/mic.svg"
+import stop from '../assets/stop.svg'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Translate = () => {
   const [languages, setLanguages] = useState([]);
@@ -18,16 +21,6 @@ const Translate = () => {
       setLanguages(res.data);
     });
   }, []);
-
-  function handleTranlateText() {
-    let response = axios.post(
-      `https://api.mymemory.translated.net/get?q=${text}&langpair=${sourecText}|${targetText}`
-    );
-    response.then(function (res) {
-        setTargetLanguage(res.data.matches[0].target)
-      setTranslatedText(res.data.responseData.translatedText);
-    });
-  }
 
   function handleChange(e) {
     setText(e.target.value);
@@ -46,14 +39,37 @@ const Translate = () => {
     speechSynthesis.speak(utterance)
   }
 
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  function handleTranlateText() {
+    let response = axios.post(
+      `https://api.mymemory.translated.net/get?q=${transcript ? transcript : text}&langpair=${sourecText}|${targetText}`
+    );
+    response.then(function (res) {
+        setTargetLanguage(res.data.matches[0].target)
+      setTranslatedText(res.data.responseData.translatedText);
+    });
+  }
+
   return (
     <div className="container">
       <div className="main-div">
         <div className="left-div">
-          <div style={{ display: "flex", gap: "20px", marginTop: "15px" }}>
+          <div style={{ display: "flex", gap: "30px", marginTop: "15px" }}>
+          <img onClick={SpeechRecognition.startListening} width={"20px"} src={mic} alt="" />
+          <img class={`${listening ? 'filter-green' : ''}`} onClick={SpeechRecognition.stopListening} src={stop} width={"25px"} alt="" />
             <select
               onChange={(e) => setSourceText(e.target.value)}
-              value={detectedSourceText}
+              value={sourecText}
               name=""
               id=""
             >
@@ -62,11 +78,11 @@ const Translate = () => {
                 <option value={item.code}>{item.name}</option>
               ))}
             </select>
-
             <img onClick={handleSourceSpeech} width={"25px"} src={volume} alt="" />
           </div>
           <textarea
             onChange={handleChange}
+            value={transcript ? transcript : text}
             placeholder="Enter your text"
             rows={20}
             cols={60}
